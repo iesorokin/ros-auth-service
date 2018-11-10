145334edd269b9a69ae9b9913998e82eb3a1f7d2
Ivan <Ivan.Sorokin@leroymerlin.ru>
2018-11-10 21:21:00 +0300

    first init

---
+++ src/main/kotlin/com/anti/mrg/rosauthservice/RosAuthServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.anti.mrg.rosauthservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RosAuthServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RosAuthServiceApplication>(*args)
+}
